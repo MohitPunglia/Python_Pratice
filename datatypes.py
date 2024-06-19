e563f0387fdd23d559510e3c3a6645ee58d58a4c
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-06-19 21:50:38 +0100

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+#Basic type - Integer, Float, Complex, Boolean and String 
+#Container type - List, Tuples, Sets and Dictionary
+#User defined types - Class
+
+#Integer
+print(4)
+print(1e308)
+
+#complex
+print(4+5j)
+
+#Float
+print(3.4)
+print(1.7e308)
+
+#Boolean
+print(True)
+print(False)
+
+#String
+print('Mohit')
+print("Mohit")
+print("""Mohit""")
+
+#List
+print([1,2,3,4,5])
+
+#tuples
+print((1,2,3,4,5))
+
+#Set
+print({1,2,3,4,5})
+
+#Dictinary
+print({"Name":"Mohit","Gender":"Male","Age":"30"})
+
