67684f2fc7f13816d663ca1644eb815b5a577d02
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-07-04 23:12:52 +0100

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,9 @@
+# WAP to convert temperature from celcius to fahrenheit
+
+temp=float(input("Enter temperature in celcius : "))
+
+fahrenheit=(temp*1.8)+32
+
+print('fahrenheit temperature is ', fahrenheit )
+
+print(type(fahrenheit))
