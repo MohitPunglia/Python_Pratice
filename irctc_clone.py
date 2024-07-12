381bee1de69b24f2084d4d245afc8e5899aa8d0b
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-07-12 23:36:48 +0100

    Create irctc_clone.py

---
+++ irctc_clone.py
@@ -0,0 +1,17 @@
+#import requests
+from tkinter import *
+
+'''print ("Test started")
+
+data=requests.get("https://reqres.in/api/users/2")
+
+data=data.json()
+
+print(data)'''
+
+root=Tk()
+
+root.title("Login")
+
+root.mainloop()
+
