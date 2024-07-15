8cb366274d19dc826e4318af2d12c0782f42c2ad
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-07-16 00:40:28 +0100

    Create volume_cylinder.py

---
+++ volume_cylinder.py
@@ -0,0 +1,12 @@
+# WAP to find volume of cylinder
+# V=πr2h
+
+radius=int(input("Enter Radius : "))
+
+height=int(input("Enter height : "))
+
+pie=3.14
+
+volume=pie * (radius*2) * height
+
+print("Volume of cylinder is : ", volume)
